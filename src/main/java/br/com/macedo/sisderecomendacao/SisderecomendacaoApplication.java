fed62d693c911f85b8ea8c72443c676900e89ee7
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-03-21 10:51:09 -0400

    Projeto criado

---
+++ src/main/java/br/com/macedo/sisderecomendacao/SisderecomendacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.macedo.sisderecomendacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisderecomendacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisderecomendacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/macedo/sisderecomendacao/SisderecomendacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.macedo.sisderecomendacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisderecomendacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
