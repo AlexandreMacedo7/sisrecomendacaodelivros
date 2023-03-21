bf90aa7025006e5eba66b4117ab0545fd760234d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2023-03-21 11:36:51 -0400

    Repository criados, pom.xml autalizado para retirda de dependencia incorreta

---
+++ src/main/java/br/com/macedo/sisderecomendacao/repository/LivroRepository.java
@@ -0,0 +1,10 @@
+package br.com.macedo.sisderecomendacao.repository;
+
+import br.com.macedo.sisderecomendacao.model.Aluno;
+import br.com.macedo.sisderecomendacao.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
